385f01838165c66a407a3c5f22f7dee856eebc3d
Weston M. Binford III <wbinford@gmail.com>
2013-06-14 15:26:35 -0500

    Added Sort extension method to Record

---
+++ src/EtlGate/RecordExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EtlGate
+{
+	public static class RecordExtensions
+	{
+		public static IEnumerable<Record> Sort(this IEnumerable<Record> input, IRecordKeyComparer comparer)
+		{
+			return input.OrderBy(x => x, comparer);
+		}
+	}
+}
